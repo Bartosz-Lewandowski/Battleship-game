e519b1c0b5966ef9edeb806b5db4bc23a5233838
Bartosz-Lewandowski <112126@student.upwr.edu.pl>
2020-12-10 13:58:36 +0100

    Add function to check validate of ship

--- set_ships.py
+++ set_ships.py
@@ -136,12 +136,14 @@ def draw_matrix(board_size):
         matrix[i[0]-1,i[1]-1] = 1
     return matrix
 
-print('Ułożenie statków dla planszy o rozmiarze 9:\n')
-print(draw_matrix(9))
-print('\n\n')
-print('Ułożenie statków dla planszy o rozmiarze 12:\n')
-print(draw_matrix(12))
-print('\n\n')
-print('Ułożenie statków dla planszy o rozmiarze 15:\n')
-print(draw_matrix(15))
-print('\n\n')
+
+test_ship = [(1, 1), (1, 2), (1, 3)] 
+test_taken = [(3, 7), (3, 5), (4, 6), (2, 6), (3, 8), (3, 6), (4, 7), (2, 7), (3, 9), (4, 8), (2, 8), (6, 8), (7, 9), (5, 9), (7, 8), (8, 9), (6, 9), (8, 8), (9, 9)]
+def check_validate(taken_spots, ship):
+    if any(elem in ship for elem in taken_spots):
+        print('Ship is not validate')
+    else:
+        print('Ship is validate')
+
+check_validate(test_taken,test_ship)
+
